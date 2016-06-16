65969a5df671a1ab376c22a8bff80f091cd01bfc
hoangnl <nguyenlehoang@hotmail.com>
2016-06-16 20:17:32 +0700

    WCF

---
+++ DI/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace wcf_ninject.DI
+{
+    public interface IRepository
+    {
+        int CountUsers();
+    }
+
+    public class LinqToSQLRepository : IRepository
+    {
+        public int CountUsers()
+        {
+            return 10;
+        }
+    }
+
+    public class FakeRepository : IRepository
+    {
+        public int CountUsers()
+        {
+            return 1000000;
+        }
+    }
+}
---
+++ Service1.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using wcf_ninject.DI;
+
+namespace wcf_ninject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    public class Service1 : IService1
+    {
+        private readonly IRepository _repo;
+
+        public Service1(IRepository repository)
+        {
+            _repo = repository;
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0} _ {1}", value, _repo.CountUsers());
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
